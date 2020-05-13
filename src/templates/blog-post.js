38c2d1bef20ebc3a0e4a0b3152c9064785097b42
joypauls <joeypaulsen3@gmail.com>
2020-05-13 01:35:26 -0500

    put katex in

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -6,6 +6,9 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm, scale } from "../utils/typography"
 
+// for katex
+require(`katex/dist/katex.min.css`)
+
 const BlogPostTemplate = ({ data, pageContext, location }) => {
   const post = data.markdownRemark
   const siteTitle = data.site.siteMetadata.title
